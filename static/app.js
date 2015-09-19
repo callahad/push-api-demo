8d75b16b2f522ab6007e4b27512f45ac4955a616
Dan Callahan <dan.callahan@gmail.com>
2015-09-18 21:33:19 -0500

    Refactor

--- static/app.js
+++ static/app.js
@@ -3,210 +3,129 @@
 
 "use strict";
 
-function updateStatus(what, label, okOrAction) {
-  var ok;
-  var action;
+// UI Helpers
 
-  if (typeof okOrAction === "function") {
-    ok = undefined;
-    action = okOrAction;
-  } else {
-    ok = okOrAction;
-    action = undefined;
-  }
+var ui = {};
 
-  var target = document.querySelector(".status[data-what='" + what + "']");
-
-  var elem;
-  if (action) {
-    elem = document.createElement("button");
-    elem.appendChild(document.createTextNode(label));
-    elem.addEventListener("click", function(e) {
-      e.preventDefault();
-      action();
+ui.notificationPrompt = function() {
+  return new Promise(function(resolve, reject) {
+    console.debug("Displaying notification UI");
+    Notification.requestPermission(function(permission) {
+      console.debug("Hiding notification UI");
+      return resolve(permission);
     });
-  } else {
-    elem = document.createTextNode(label);
-  }
-
-  while (target.firstChild) { target.removeChild(target.firstChild); }
-
-  target.dataset.disposition = ok;
-  target.appendChild(elem);
-}
-
-function checkCapabilities() {
-  return new Promise(function(resolve, reject) {
-    var errors = [];
-
-    // Service Worker
-    if (!("serviceWorker" in navigator)) {
-      updateStatus("serviceworker", "unsupported", false);
-      errors.push(new Error("Service Workers not supported"));
-    } else {
-      updateStatus("serviceworker", "supported");
-    }
-
-    // Service Worker Notifications
-    if (!("showNotification" in ServiceWorkerRegistration.prototype)) {
-      updateStatus("swnotifications", "unsupported", false);
-      errors.push(new Error("Notifications not supported in Service Workers"));
-    } else {
-      updateStatus("swnotifications", "supported", true);
-    }
-
-    // Desktop Notifications
-    if (Notification.permission === "denied") {
-      updateStatus("notifications", "blocked", false);
-      errors.push(new Error("Notifications blocked"));
-    } else if (Notification.permission === "granted") {
-      updateStatus("notifications", "enabled", true);
-    } else {
-      updateStatus("notifications", "enable", function() {
-        enableNotifications()
-        .then(function() { updateStatus("notifications", "enabled", true); })
-        .catch(function() { updateStatus("notifications", "blocked", false); });
-      });
-    }
-
-    // Push API
-    if (!("PushManager" in window)) {
-      updateStatus("push", "unsupported", false);
-      errors.push(new Error("Push messages not supported"));
-    } else {
-      updateStatus("push", "supported");
-      navigator.serviceWorker.ready
-      .then(function(reg) {
-        return reg.pushManager.permissionState();
-      })
-      .then(function(permission) {
-        switch (permission) {
-          case "granted":
-            return subscribe();
-          case "prompt":
-            return false;
-          case "denied":
-            throw new Error("Push notifications are blocked");
-          default:
-            throw new Error("Unexpected error checking Push permission");
-        }
-      })
-      .then(function(subscribed) {
-        if (subscribed) {
-          updateStatus("push", "subscribed", true);
-        } else {
-          updateStatus("push", "subscribe", function() {
-            subscribe()
-            .then(function() { updateStatus("push", "subscribed", true); })
-            .catch(function() { updateStatus("push", "subscription failed", false); });
-          });
-        }
-      })
-      .catch(function(e) {
-        updateStatus("push", "error", false);
-        console.error(e);
-      });
-    }
-
-    if (errors.length) {
-      errors.forEach(function (error) { console.error(error); });
-      return reject("Missing required APIs, see console for details.");
-    }
-
-    return resolve(true);
   });
-}
+};
 
-function registerWorker() {
-  return navigator.serviceWorker.register("./serviceworker.js")
-  .then(function() { updateStatus("serviceworker", "active", true); })
-  .catch(function(e) { updateStatus("serviceworker", "failed", false) ; console.error(e); });
-}
+ui.pushPrompt = function(opts) {
+  return navigator.serviceWorker.ready
+    .then(function(reg) {
+      console.debug("Displaying push UI");
+      return reg;
+    })
+    .then(function(reg) {
+      return Promise.all([reg, reg.pushManager.subscribe(opts)]);
+    })
+    .then(function(results) {
+      console.debug("Hiding push UI on success");
+    })
+    .catch(function(err) {
+      console.debug("Hiding push UI on error");
 
-function enableNotifications() {
-  return new Promise(function(resolve, reject) {
-    Notification.requestPermission(function(result) {
-      switch (result) {
-        case "granted":
-          return resolve(result);
-        case "denied":
-          return reject(new Error("Notification request denied"));
-        case "default":
-          return reject(new Error("Notification request dismissed"));
-        default:
-          return reject(new Error("Unexpected error enabling notifications"));
+      // Bug 1206302: Firefox rejects with a string instead of DOMException
+      var DENIED = "PermissionDeniedError";
+      if (err.message === DENIED || err === DENIED) {
+        return "denied";
       }
+
+      // Something unexpected failed. Panic.
+      // e.g., Chrome currently throws if the app isn't registered in GCM
+      throw err;
     });
-  });
+};
+
+// App Logic
+
+function verifySupport() {
+  function assert(prop, message) {
+    if (!prop) { throw new Error(message); }
+    return true;
+  }
+
+  assert(self.fetch, "Fetch is not supported");
+  assert(navigator.serviceWorker, "Service Worker is not supported");
+  assert(self.ServiceWorkerRegistration.prototype.showNotification, "ServiceWorker Notifications are not supported");
+  assert(self.Notification, "Notifications are not supported");
+  assert(self.PushManager, "Push is not supported");
+
+  return true;
+}
+
+function enableNotifications(promptFn, permission) {
+  if (!permission) { return enableNotifications(promptFn, Notification.permission); }
+
+  switch (permission) {
+    case "granted":
+      return Promise.resolve(true);
+    case "denied":
+      return Promise.reject(new Error("Notifications blocked"));
+    case "default":
+      return promptFn().then(enableNotifications.bind(null, promptFn));
+    default:
+      return Promise.reject(new Error("Unrecognized Notification permission: " + permission));
+  }
+}
+
+function enablePush(promptFn, permission) {
+  var opts = { userVisibleOnly: true };
+
+  if (!permission) {
+    return navigator.serviceWorker.ready
+      .then(function(reg) { return reg.pushManager.permissionState(opts); })
+      .then(function(state) { return enablePush(promptFn, state); });
+  }
+
+  switch (permission) {
+    case "granted":
+      return Promise.resolve(true);
+    case "denied":
+      return Promise.reject(new Error("Push blocked"));
+    case "prompt":
+      return promptFn(opts).then(enablePush.bind(null, promptFn));
+    default:
+      return Promise.reject(new Error("Unrecognized Push permission: " + permission));
+  }
 }
 
 function subscribe() {
   return navigator.serviceWorker.ready
-  .then(function(reg) {
-    return Promise.all([reg, reg.pushManager.getSubscription()]);
-  })
-  .then(function(results) {
-    var reg = results[0];
-    var sub = results[1];
-    return sub || reg.pushManager.subscribe({ userVisibleOnly: true });
-  })
-  .then(function(sub) {
-    return fetch(window.location.origin, {
-      method: "PUT",
-      body: JSON.stringify({ user: "Bob", endpoint: sub.endpoint }),
-      headers: new Headers({ "Content-Type": "application/json" }),
-      mode: "same-origin"
+    .then(function(reg) {
+      return reg.pushManager.subscribe();
+    })
+    .then(function(sub) {
+      return fetch(window.location.origin, {
+        mode: "same-origin",
+        method: "PUT",
+        headers: new Headers({ "Content-Type": "application/json" }),
+        body: JSON.stringify({ user: "FIXME", endpoint: sub.endpoint }) // TODO: Generate unique ID
+      });
     });
-  })
-  .then(function(res) {
-    if (!res.ok) { throw new Error("Unable to subscribe"); }
-
-    console.log("Subscribed!");
-    return true;
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
 }
 
-function unsubChat() {
-  return fetch(window.location.origin, {
-    method: "DELETE",
-    body: JSON.stringify({ user: "Bob" }),
-    headers: new Headers({ "Content-Type": "application/json" }),
-    mode: "same-origin"
-  })
-  .then(function(res) {
-    if (!res.ok) { throw new Error("Unable to unsubscribe from chat"); }
+Promise.resolve()
+  .then(verifySupport)
+  .then(function() { console.info("Browser support OK"); })
 
-    console.log("Unsubscribed from chat!");
-  });
-}
+  .then(function() { return navigator.serviceWorker.register("./serviceworker.js"); })
+  .then(function() { console.info("ServiceWorker installation OK"); })
 
-function unsubPush() {
-  return navigator.serviceWorker.ready
-  .then(function(reg) {
-    return reg.pushManager.getSubscription();
-  }).then(function(sub) {
-    return sub ? sub.unsubscribe() : true;
-  }).then(function(success) {
-    if (!success) { throw new Error("Unable to unsubscribe from push service"); }
+  .then(function() { return enableNotifications(ui.notificationPrompt); })
+  .then(function() { console.info("Notifications OK"); })
 
-    console.log("Unsubscribed from push service!");
-  });
-}
+  .then(function() { return enablePush(ui.pushPrompt); })
+  .then(function() { console.info("Push OK"); })
 
-function unsubscribe() {
-  return Promise.all([unsubChat(), unsubPush()])
-  .then(function() {
-    console.log("Fully unsubscribed!");
-  }).catch(function(err) {
-    console.error(err);
-  });
-}
+  .then(subscribe)
+  .then(function() { console.info("Subscribed OK"); })
 
-checkCapabilities()
-.then(registerWorker)
-.catch(function (err) {
-  console.log(err);
-});
+  .catch(function(err) { console.error(err.message || err); });
